4fd152d36139805fe856f44ed07cb65ff2eccf99
Collin Murdock <collinmurdock22@gmail.com>
2017-01-22 15:47:48 -0500

    fixed calendar height

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@ function logIt(it){
 	$(".log-content").append("<p>"+it+"</p>");
 }
 
+var calendarWidth = parseInt($(".calendar-container").css("width"));
+var calendarHeight = calendarWidth * 0.6;
+$(".calendar-container").css("height", calendarHeight);
+
+for(i=0; i<7; i++){
+	$(".row").append('<div class="box"></div>');
+}
